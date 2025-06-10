9a6b9ae6468416001cde8c4f56ed2f7d51178ae9
chinmareno <chinmareno1@gmail.com>
2025-06-11 03:17:38 +0700

    refactor(navbar): move HideOnScrollWrapper out of Navbar component

--- src/app/(root)/_components/Navbar/index.tsx
+++ src/app/(root)/_components/Navbar/index.tsx
@@ -1,6 +1,5 @@
 import { LanguageSelector } from "~/components/LanguageSelector";
 import { Button } from "~/components/ui/button";
-import { HideOnScrollWrapper } from "~/components/wrapper/HideOnScrollWrapper";
 import XiaoErpLogo from "~/components/XiaoErpLogo";
 import { useLanguageStore } from "~/hooks/useLanguageStore";
 
@@ -8,39 +7,37 @@ export const Navbar = () => {
   const { selectedLanguage, setSelectedLanguage } = useLanguageStore();
 
   return (
-    <HideOnScrollWrapper>
-      <nav className="border-b bg-gray-100">
-        <div className="grid w-full grid-cols-3 items-center justify-between px-4 py-3.5 sm:px-6 lg:px-8">
-          {/* Left Navbar */}
-          <div>
-            <button className="flex bg-amber-400 hover:cursor-pointer">
-              <XiaoErpLogo />
-            </button>
-          </div>
-          {/* Middle Navbar */}
-          <div className="bg-bekgron flex justify-center gap-10">
-            <div>Menu</div>
-            <div>Menu</div>
-            <div>Menu</div>
-          </div>
-          {/* Right Navbar */}
-          <div className="flex justify-end">
-            <Button
-              variant="outline"
-              className="hidden border-none bg-transparent shadow-none hover:cursor-pointer hover:text-zinc-600 sm:inline-flex"
-            >
-              Login
-            </Button>
-            <Button className="bg-primary hover:bg-primary/90 mr-6 hover:cursor-pointer">
-              Sign Up
-            </Button>
-            <LanguageSelector
-              selectedLanguage={selectedLanguage}
-              setSelectedLanguage={setSelectedLanguage}
-            />
-          </div>
+    <nav className="border-b bg-gray-100">
+      <div className="grid w-full grid-cols-3 items-center justify-between px-4 py-3.5 sm:px-6 lg:px-8">
+        {/* Left Navbar */}
+        <div>
+          <button className="flex bg-amber-400 hover:cursor-pointer">
+            <XiaoErpLogo />
+          </button>
         </div>
-      </nav>
-    </HideOnScrollWrapper>
+        {/* Middle Navbar */}
+        <div className="bg-bekgron flex justify-center gap-10">
+          <div>Menu</div>
+          <div>Menu</div>
+          <div>Menu</div>
+        </div>
+        {/* Right Navbar */}
+        <div className="flex justify-end">
+          <Button
+            variant="outline"
+            className="hidden border-none bg-transparent shadow-none hover:cursor-pointer hover:text-zinc-600 sm:inline-flex"
+          >
+            Login
+          </Button>
+          <Button className="bg-primary hover:bg-primary/90 mr-6 hover:cursor-pointer">
+            Sign Up
+          </Button>
+          <LanguageSelector
+            selectedLanguage={selectedLanguage}
+            setSelectedLanguage={setSelectedLanguage}
+          />
+        </div>
+      </div>
+    </nav>
   );
 };
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { HideOnScrollWrapper } from "~/components/wrapper/HideOnScrollWrapper";
 import { Footer } from "./_components/Footer";
 import { Hero } from "./_components/Hero";
 import { Navbar } from "./_components/Navbar";
@@ -8,7 +9,9 @@ import { ModulesOverview } from "./_components/Overview";
 const LandingPage = () => {
   return (
     <div className="min-h-screen">
-      <Navbar />
+      <HideOnScrollWrapper>
+        <Navbar />
+      </HideOnScrollWrapper>
       <Hero />
       <ModulesOverview />
       <Footer />
